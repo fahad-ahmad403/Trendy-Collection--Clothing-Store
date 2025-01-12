79ec7caeb5273c7f08a591d5e370b9d2e523575e
Fahad Ahmad <fahadahmad48224@gmail.com>
2025-01-12 11:48:47 +0500

    file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Trendy-Collection/",
+  base: "/Trendy-Collection--Clothing-Store/",
   server: {
     host: true,
   },
